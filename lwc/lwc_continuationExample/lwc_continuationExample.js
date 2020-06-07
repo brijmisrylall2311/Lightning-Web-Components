e3793b769bac5f241f3c59c51d441b1738e0bccd
brijmisrylall <brij.misrylall@gmail.com>
2020-06-07 00:19:40 -0400

    Continuation callout example

---
+++ lwc/lwc_continuationExample/lwc_continuationExample.js
@@ -0,0 +1,42 @@
+import { LightningElement, api } from 'lwc';
+import startRequest from '@salesforce/apexContinuation/AP_Continuation.startRequest';
+
+export default class ContinuationTest extends LightningElement {
+
+    @api myResponse;
+    @api showResponse1 = false;
+    @api showResponse2 = false;
+    @api showResponse3 = false;
+    @api isLoaded = false;
+    @api disableButton = false;
+
+    connectedCallback(){
+        this.isLoaded= true;
+    }
+    
+    makeCallout(){
+        this.isLoaded = false;
+        startRequest()
+            .then(result => {
+                console.log("webservice response");
+                console.log(result);
+                this.myResponse = result;
+                if(result.Continuation1){
+                    this.showResponse1 = true;
+                }
+                if(result.Continuation2){
+                    this.showResponse2 = true;
+                }
+                if(result.Continuation3){
+                    this.showResponse3 = true;
+                }
+                this.isLoaded = true;
+                this.disableButton = true;
+            })
+            .catch(error => {
+                console.log("error call");
+                console.log("error >> " + JSON.parse(this.error));
+            });
+    }
+
+}
